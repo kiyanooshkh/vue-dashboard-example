6c84631c8dd3a2e843ee120190fd88b09e7a31f4
Kiyanoosh Khosravi <Kiyanoosh.Khosravi@certegy.com.au>
2019-08-21 08:44:49 +0930

    adding date picker

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -10,25 +10,105 @@
           <h5>Add a New Project</h5>
         </v-card-title>
 
-        <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</v-card-text>
+        <v-card-text>
+          <v-form v-model="valid">
+            <v-container>
+              <v-row>
+                <v-col cols="12">
+                  <v-text-field
+                    v-model="title"
+                    :rules="nameRules"
+                    :counter="20"
+                    label="Title"
+                    prepend-icon="folder"
+                    required
+                  ></v-text-field>
+                </v-col>
+
+                <v-col cols="12">
+                  <v-textarea
+                    v-model="content"
+                    :rules="nameRules"
+                    :counter="200"
+                    label="Information"
+                    prepend-icon="edit"
+                    required
+                  ></v-textarea>
+                </v-col>
+
+                <v-col cols="12">
+                  <v-menu
+                    ref="menu"
+                    v-model="menu"
+                    :close-on-content-click="false"
+                    :return-value.sync="due"
+                    transition="scale-transition"
+                    offset-y
+                    full-width
+                    min-width="290px"
+                  >
+                    <template v-slot:activator="{ on }">
+                      <v-text-field
+                        :value="formattedDate"
+                        label="Picker in menu"
+                        prepend-icon="event"
+                        readonly
+                        v-on="on"
+                      ></v-text-field>
+                    </template>
+                    <v-date-picker v-model="due" no-title scrollable>
+                      <v-spacer></v-spacer>
+                      <v-btn text color="primary" @click="menu = false">Cancel</v-btn>
+                      <v-btn text color="primary" @click="$refs.menu.save(due)">OK</v-btn>
+                    </v-date-picker>
+                  </v-menu>
+                </v-col>
+
+                <v-col cols="12">
+                  <v-btn class="mr-2" depressed color="success" @click="submit">
+                    <v-icon left>save</v-icon>
+                    <span class="font-weight-light">Add new project</span>
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-form>
+        </v-card-text>
 
         <v-divider></v-divider>
 
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="primary" text @click="dialog = false">I accept</v-btn>
-        </v-card-actions>
+        <v-card-actions></v-card-actions>
       </v-card>
     </v-dialog>
   </div>
 </template>
 
 <script>
+import format from 'date-fns/format';
+
 export default {
   data() {
     return {
+      valid: false,
+      due: null,
+      title: "",
+      content: "",
+      nameRules: [
+        v => !!v || "Name is required",
+        v => v.length <= 10 || "Name must be less than 10 characters"
+      ],
       dialog: false
     };
+  },
+  methods: {
+    submit() {
+     // console.log(this.title, this.content);
+    }
+  },
+  computed:{
+      formattedDate(){
+          return this.due? format(this.due, 'Do MMM YYY'):'';
+      }
   }
 };
 </script>
