ef090e65bf578b1f9b3f3e1a41a0d66334c4ce5e
Kiyanoosh Kh <khosravi.kiyanoosh@gmail.com>
2020-09-15 10:59:25 +0930

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-dashboard-example/'
+    ? '/dashboard-vue-example/'
     : '/',
     transpileDependencies: ['vuetify']
   }
